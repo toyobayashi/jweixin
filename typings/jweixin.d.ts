144de9d0a4ed7ba55c70d28a19d265ab66425659
toyobayashi <lifenglin314@outlook.com>
2020-06-22 17:13:01 +0800

    jsApiList

--- typings/jweixin.d.ts
+++ typings/jweixin.d.ts
@@ -111,7 +111,7 @@ declare namespace wx {
     /**
      * 需要使用的JS接口列表
      */
-    jsApiList?: string[];
+    jsApiList?: Method[];
     /**
      * 开启调试模式,调用的所有api的返回值会在客户端alert出来，
      * 若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
