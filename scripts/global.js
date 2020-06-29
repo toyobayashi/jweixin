b5c272b1e7af95069f7a3b864244edfe9c58b07f
toyobayashi <lifenglin314@outlook.com>
2020-06-29 17:44:49 +0800

    1.6.10005

--- scripts/global.js
+++ scripts/global.js
@@ -1,14 +1,17 @@
 (function (_this) {
-  if (typeof globalThis !== "undefined") return globalThis;
-  if (typeof __webpack_modules__ === 'undefined') {
-    if (typeof global !== "undefined") return global;
-  }
-  if (typeof window !== "undefined") return window;
-  if (typeof self !== "undefined") return self;
   var g;
-  g = (function () { return _this; })();
+  g = (function () { return this; })();
+
   try {
     g = g || new Function("return this")();
-  } catch (_) {}
+  } catch (_) {
+    if (typeof globalThis !== "undefined") return globalThis;
+    if (typeof __webpack_modules__ === "undefined") {
+      if (typeof global !== "undefined") return global;
+    }
+    if (typeof window !== "undefined") return window;
+    if (typeof self !== "undefined") return self;
+  }
+
   return g || _this || {};
 })(this)
